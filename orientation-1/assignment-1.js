295a4ab18723593072e3a0e492047e370dc9d185
Aman Yadav <ay.work07@gmail.com>
2025-08-03 14:58:34 +0530

    assignment-1

---
+++ orientation-1/assignment-1.js
@@ -0,0 +1,39 @@
+import crypto from "crypto";
+
+function myfunction (){
+let i = 0
+
+for (i = 1; i > 0;){
+
+    const input = `100xDevs ${Math.random() * 300}`
+    const hash = crypto.createHash('sha256').update(input).digest('hex');
+    console.log(input)
+    console.log(hash)
+    if (hash.startsWith('0000')){
+    console.log('Found : ', hash)
+    break;
+    } 
+}
+}
+myfunction();
+
+// Function to find an input string that produces a hash starting with '00000'
+function findHashWithPrefix(prefix) {
+    let input = 0;
+    while (true) {
+        let inputStr = input.toString();
+        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
+        if (hash.startsWith(prefix)) {
+            return { input: inputStr, hash: hash };
+        }
+        input++;
+    }
+}
+
+// Find and print the input string and hash
+const result = findHashWithPrefix('00000');
+console.log(`Input: ${result.input}`);
+console.log(`Hash: ${result.hash}`);
+
+
+
