8b206d29ffff4661e95a1d6bcbeecdd8bb8ab9f1
Aman Yadav <ay.work07@gmail.com>
2025-08-13 23:36:54 +0530

    done

--- public-key-cryptography/index.js
+++ public-key-cryptography/index.js
@@ -1,14 +1,4 @@
-//bit
-const x = 3;
-console.log("bit:",x)
 
-//byte
-const y = 202;
-console.log("Byte: ", y)
-
-//array of bytes
-const z = [202, 304, 105, 255, 256]
-console.log("Bytes array: ", z)
 
 let bytes = new Uint8Array([0, 255, 256, 3, 400]);
 
@@ -32,5 +22,45 @@ function asciiToBytes(asciiString) {
 
 // Example usage:
 const ascii = "Hello";
-const byteArray = asciiToBytes(ascii);
-console.log(byteArray); // Output: [72, 101, 108, 108, 111]
+let byteArray = asciiToBytes(ascii);
+console.log(byteArray); 
+// Output: [72, 101, 108, 108, 111]
+function arrayToHex(byteArray) {
+  let hexString = '';
+  for (let i = 0; i < byteArray.length; i++) {
+    hexString += byteArray[i].toString(16).padStart(2, '0');
+  }
+  return hexString;
+}
+
+// Example usage:
+byteArray = new Uint8Array([72, 101, 108, 108, 111]); // Corresponds to "Hello"
+const hexString = arrayToHex(byteArray);
+console.log(hexString); // Output: "48656c6c6f"
+
+function hexToArray(hexString) {
+  const byteArray = new Uint8Array(hexString.length / 2);
+  for (let i = 0; i < byteArray.length; i++) {
+    byteArray[i] = parseInt(hexString.substr(i * 2, 2), 16);
+  }
+  return byteArray;
+}
+
+// Example usage:
+const hex = "48656c6c6f";
+const byteArrayFromHex = hexToArray(hex);
+console.log(byteArrayFromHex); // Output: Uint8Array(5) [72, 101, 108, 108, 111]
+
+const uint8Array = new Uint8Array([72, 101, 108, 108, 111]);
+const base64Encoded = Buffer.from(uint8Array).toString("base64");
+console.log(base64Encoded);
+
+// import bs58 from 'bs58'
+// function base58ToUint8Array(base58String) {
+//   return bs58.decode(base58String);
+// }
+
+// // Example usage:
+// const base58 = base58String; // Use the previously encoded Base58 string
+// const byteArrayFromBase58 = base58ToUint8Array(base58);
+// console.log(byteArrayFromBase58); // Output: Uint8Array(5) [72, 101, 108, 108, 111]
